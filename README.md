5e250b70c9d27bc1149aa1300af0c48385305783
Lei Wang <lei4.wang@intel.com>
2024-06-11 23:19:19 -0400

    feat: align for each part in the entry

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lspmark
 
+plenary is a must dependency.
+
  - Lazy calibrating: calibrate the symbols in that buffer only when entering it.
--- lua/telescope/_extensions/lspmark.lua
+++ lua/telescope/_extensions/lspmark.lua
@@ -12,12 +12,17 @@ function M.lspmark(opts)
 	local results = {}
 	local protocol = vim.lsp.protocol
 
+	local max_file_name_len, max_kind_len, max_symbol_len = 0, 0, 0
 	for file_name, kinds in pairs(bookmarks.bookmarks) do
+		max_file_name_len = math.max(string.len(file_name:match("^.+/(.+)$")), max_file_name_len)
 		for kind, symbols in pairs(kinds) do
+			kind = protocol.SymbolKind[tonumber(kind)] or "Unknown"
+			max_kind_len = math.max(string.len(kind), max_kind_len)
 			for name, range in pairs(symbols) do
+				max_symbol_len = math.max(string.len(name), max_symbol_len)
 				table.insert(results, {
 					filename = file_name,
-					kind = protocol.SymbolKind[tonumber(kind)] or "Unknown",
+					kind = kind,
 					lnum = range[1] + 1,
 					col = range[3],
 					text = name,
@@ -26,23 +31,25 @@ function M.lspmark(opts)
 		end
 	end
 
+	local formatted_str = "%-"
+		.. max_file_name_len
+		.. "s:"
+		.. "%4d:%-3d [%-"
+		.. max_kind_len
+		.. "s] %-"
+		.. max_symbol_len
+		.. "s"
+
 	pickers.new(opts, {
 		prompt_title = "Bookmarks",
 		finder = finders.new_table({
 			results = results,
 			entry_maker = function(entry)
-				local filename = entry.filename:match("^.+/(.+)$")
+				local file_name = entry.filename:match("^.+/(.+)$")
 				return {
 					value = entry,
-					display = string.format(
-						"%s:%d:%d: [%s] %s",
-						filename,
-						entry.lnum,
-						entry.col,
-						entry.kind,
-						entry.text
-					),
-					ordinal = entry.text .. entry.kind .. filename,
+					display = string.format(formatted_str, file_name, entry.lnum, entry.col, entry.kind, entry.text),
+					ordinal = entry.text .. entry.kind .. file_name,
 					filename = entry.filename,
 					lnum = entry.lnum,
 					col = entry.col,
